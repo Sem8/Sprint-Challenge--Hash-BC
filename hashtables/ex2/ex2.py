76a38baa9c47cbc77ec02987a486f4f914da2ce7
Sem8 <mssemmi8@gmail.com>
2019-08-09 14:06:01 -0400

    example 2 of hashtable assignment complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -15,16 +15,18 @@ class Ticket:
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
-
     # route is the array
     # hashtable is the hashtable
+
+    # Make a hashtable with the source and destination info.
     for i in range(length):
         hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
-        if tickets[i].source == None:
-            route.insert(0, tickets[i].destination)
-        if tickets[i].destination == None:
-            route.insert(length-1, tickets[i].source)    
 
-
+    # Set first value of from source 'NONE' key into the route array
+    route[0] = hash_table_retrieve(hashtable, 'NONE')
+    
+    # iterate thru the rest of array after initial index in route, and set value from key obtained from previous value     
+    for i in range(1, length):
+        route[i] = hash_table_retrieve(hashtable, route[i-1])
 
     return route
